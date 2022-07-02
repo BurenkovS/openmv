056cc372e6b8597425ab911aeb392bc40ab75fbc
burenkov <burenkov@list.ru>
2022-07-02 22:28:29 +0300

    Add stereocamera support, add double width for QVGA resolution

---
+++ src/omv/sensors/h7stereocam.h
@@ -0,0 +1,8 @@
+/*
+ * H7STEREOCAM driver.
+ */
+#ifndef __H7STEREOCAM_H__
+#define __H7STEREOCAM_H__
+#define H7STEREOCAM_XCLK_FREQ 54000000
+int h7stereocam_init(sensor_t *sensor);
+#endif // __H7STEREOCAM_H__
